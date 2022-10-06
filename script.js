afba32caf8475dc44f5013821c98addc1b25a45d
Alexis Herrera <lexibeli@Alexiss-iMac.local>
2022-10-05 23:50:15 -0400

    in process

---
+++ script.js
@@ -0,0 +1,45 @@
+let data = [];
+let random = [];
+let imageFront = document.querySelector(".digi-front")
+
+const url = "https://digimon-api.vercel.app/api/digimon";
+
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    renderResults(response)
+  })
+
+function renderResults(response) {
+
+  data = response.slice(0)
+  random = response.sort(() => Math.random() - Math.random()).slice(0, 8)
+
+
+  random = random.concat(random)
+
+  createBoard(random)
+
+}
+
+
+function createBoard(random) {
+  let cardContainer = document.querySelectorAll('.card')
+
+  random.forEach((random, index) => {
+
+    let htmlTemplate = `
+    <div class="card-back card-face">
+    <img class="digi-back" src="https://pbs.twimg.com/media/EcJJNZEX0AES_Wh.jpg">
+    </div>
+     <div class="card-front card-face">
+    <img class="digi-front" src="${random.img}">
+    </div>
+    `;
+    cardContainer[index].insertAdjacentHTML('afterbegin', htmlTemplate)
+  })
+
+}
